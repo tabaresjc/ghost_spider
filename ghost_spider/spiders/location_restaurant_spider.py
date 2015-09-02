2adbdd53df295719c0d8f2d60aa2d4c85a603b75
Juan Tabares <juan.ctt@live.com>
2015-09-02 12:05:49 +0900

    more prefectures

--- ghost_spider/spiders/location_restaurant_spider.py
+++ ghost_spider/spiders/location_restaurant_spider.py
@@ -15,8 +15,8 @@ class LocationRestaurantSpider(BaseSpider):
   allowed_domains = ["localhost", "search.loco.yahoo.co.jp", "loco.yahoo.co.jp"]
   target_base_url = "http://search.loco.yahoo.co.jp"
   start_urls = (URLS['niigata'] + URLS['toyama'] + URLS['ishikawa'] + URLS['fukui'] + URLS['yamanashi']
-    + URLS['nagano'] + URLS['gifu'] + URLS['shizuoka'] + URLS['aichi'] + URLS['ishikawa'] + URLS['ishikawa']
-    + URLS['ishikawa'] + URLS['ishikawa'])
+    + URLS['nagano'] + URLS['gifu'] + URLS['shizuoka'] + URLS['aichi'] + URLS['mie'] + URLS['shiga']
+    + URLS['nara'] + URLS['wakayama'])
   count = 0
   total = 0
   scan_mode = False
